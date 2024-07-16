a4a35a19c439ecbccc6b31b52ad30fb4821a34e6
Akamatsu Shinichiro <e1n21002@oit.ac.jp>
2024-07-16 14:48:47 +0900

    モンスターにレベル概念を追加

--- app/src/main/java/org/example/Monster.java
+++ app/src/main/java/org/example/Monster.java
@@ -4,10 +4,16 @@ public class Monster {
   // test
   private String name;
   private int rare;
+  private int level;
 
   public Monster(int nameNum, int rare) {
     this.name = this.Summon(nameNum);
     this.rare = rare;
+    this.level = 1;
+  }
+
+  public void levelUp() {
+    this.level++;
   }
 
   public String Summon(int nameNum) {
@@ -17,6 +23,6 @@ public class Monster {
 
   @Override
   public String toString() {
-    return "モンスター名: " + this.name + " レア度: " + this.rare;
+    return "モンスター名: " + this.name + " レア度: " + this.rare + " レベル: " + this.level;
   }
 }
