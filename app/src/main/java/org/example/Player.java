f078c19e518c6416375bd72c360ae1d783b93d73
ukyo <e1n21077@oit.ac.jp>
2024-07-16 11:11:28 +0900

    showDecksizeメソッド実装

--- app/src/main/java/org/example/Player.java
+++ app/src/main/java/org/example/Player.java
@@ -33,4 +33,8 @@ public class Player {
     public ArrayList<Monster> getMosterDeck() {
         return this.monsterDeck;
     }
+
+    public int showDecksize(){
+        return this.monsterDeck.size();
+    }
 }
